ef970415f2771b123bc61fe242c08b52590e19b8
Vasiliamus <vaskashow@gmail.com>
2021-12-07 15:05:49 +0300

    11 practic

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
@@ -21,14 +22,23 @@ public class Main {
         //Массивы гласных и согласных
         int M;
         Scanner sc = new Scanner(System.in);
-        out.println("Введите размер матрицы их диапазона от 2 до 5 включительно:");
-        M = testNum();
-//        M = sc.nextLine();
-
-        while (M > 5 || M < 2) {
-            out.println("Число выходит за пределы диапазона (от 2 до 5 включительно)");
-            M = testNum();
+        out.println("Введите число M ∈ [2;5] - размер матрицы:");
+        String input = sc.nextLine();
+        while (true) {
+            if (matchNum(input)) {
+                M = Integer.parseInt(input);
+                if (M >= 2 && M <= 5) {
+                    break;
+                } else {
+                    out.println("Ошибка. Введённое число ∉ [2 ; 5] \nПовторите ввод:");
+                    input = sc.nextLine();
+                }
+            } else {
+                out.println("Ошибка. Некорректный тип данных. \nПовторите ввод:");
+                input = sc.nextLine();
+            }
         }
+
         //Объявление квадратной матрицы
         String[][] matrix;
         //Заполнение матрицы
@@ -43,28 +53,29 @@ public class Main {
         matrixPrint(matrix);
     }
 
-    public static int testNum() {
-        Scanner sc = new Scanner(System.in);
-        while (!sc.hasNextInt()) {
-            out.println("Вы ввели не число");
-            sc = new Scanner(System.in);
-        }
-        //TODO переделать метод на основе таблицы символов
-        return sc.nextInt();
-    }
-
     public static String[][] matrixInit(int M) {
         out.println("Введите \"1\", чтобы ввести данные матрицы вручную");
         out.println("Введите \"0\", чтобы данные матрицы заполнились автоматически");
-        int v = testNum();
-        while (v != 0 && v != 1) {
-            out.println("Вы ввели число, отличное от 0 или 1");
-            out.println("Введите \"1\", чтобы ввести данные матрицы вручную");
-            out.println("Введите \"0\", чтобы данные матрицы заполнились автоматически");
-            v = testNum();
+
+        Scanner sc = new Scanner(System.in);
+        int v;
+        String input = sc.nextLine();
+        while (true) {
+            if (matchNum(input)) {
+                v = Integer.parseInt(input);
+                if (v == 1 || v == 0) {
+                    break;
+                } else {
+                    out.println("Ошибка. Введённое число не является 1 или 0. \nПовторите ввод:");
+                    input = sc.nextLine();
+                }
+            } else {
+                out.println("Ошибка. Некорректный тип данных. \nПовторите ввод:");
+                input = sc.nextLine();
+            }
         }
         String[][] matrix = new String[M][M];
-        Scanner sc = new Scanner(System.in);
+        sc = new Scanner(System.in);
         Random random = new Random();
         out.println("Элемент должен состоять из пяти английских букв.");
         for (int i = 0; i < M; i++) {
@@ -73,18 +84,27 @@ public class Main {
                     //Упрощённая реализация
                     out.println("Инициализируйте элемент " + i + " строки и " + j + " столбца");
                     matrix[i][j] = sc.nextLine();
-                    while (!matrix[i][j].matches("^[a-zA-Z]+$") && matrix[i][j].length() != 5) {
-                        sc = new Scanner(System.in);
-                        out.println("Введён неверный формат элемента матрицы");
-                        out.println("Элемент матрицы должен состоять из пяти английских букв:");
-                        matrix[i][j] = sc.nextLine();
+
+                    while (true) {
+                        if (matrix[i][j].length() == 5) {
+                            if (matchSym(matrix[i][j])) {
+                                break;
+                            } else {
+                                out.println("Ошибка. Слово должно состоять из 5 английских букв. \nПовторите ввод.");
+                                out.println("Инициализируйте элемент " + i + " строки и " + j + " столбца");
+                                matrix[i][j] = sc.nextLine();
+                            }
+                        } else {
+                            out.println("Ошибка. Длина слова должна состоять из 5 английских букв. \nПовторите ввод.");
+                            out.println("Инициализируйте элемент " + i + " строки и " + j + " столбца");
+                            matrix[i][j] = sc.nextLine();
+                        }
                     }
                 } else {
                     //Симуляция автоматического заполнения
                     StringBuilder word = new StringBuilder();
                     for (int ch_int = 0; ch_int < 5; ch_int++) {
                         word.append(alphabet[abs1(random.nextInt() % alphabet.length)]);
-                        //TODO переопределить метод @nextInt
                     }
                     matrix[i][j] =  String.valueOf(word);
                 }
@@ -93,6 +113,7 @@ public class Main {
         return matrix;
     }
 
+    //Выполнено
     public static void matrixCalc(String[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
@@ -116,6 +137,7 @@ public class Main {
         }
     }
 
+    //Выполнено
     public static void matrixPrint(String[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             out.print("| ");
@@ -126,12 +148,29 @@ public class Main {
         }
     }
 
-    public int randomInt() {
 
-    }
-
+    //Вспомогательные функции
     public static int abs1(int a) {
         return (a < 0) ? -a : a;
+    }
+
+    public static boolean matchSym(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (!((s.charAt(i) >= 65 && s.charAt(i) <= 90) || (s.charAt(i) >= 97 && s.charAt(i) <= 122))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean matchNum(String s) {
+        if (s.equals("")) return false;
+        for (int i = 0; i < s.length(); i++) {
+            if (!(s.charAt(i) >= 48 && s.charAt(i) <= 57)) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
